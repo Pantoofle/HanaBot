abac5e7a66b0303af2dd2c7bf0f466b79e94e7ff
Simon Fernandez <simon.fernandez@ens-lyon.org>
2020-09-12 18:05:00 +0200

    First playground basic functions

--- src/hanabi/playground.rs
+++ src/hanabi/playground.rs
@@ -10,13 +10,20 @@ struct Stack{
 
 struct Playground{
     color_stacks : HashMap<card::Color, Stack>,
-
+    hint_tokens : u8,
+    error_tokens : u8,
+    discard_pile : Vec<card::Card>,
+    draw_pile : Vec<card::Card>,
 }
 
 impl Playground{
     pub fn new() -> Playground{
         let p = Playground{
             color_stacks : HashMap::new(),
+            hint_tokens : 8,
+            error_tokens : 3,
+            discard_pile : Vec::<card::Card>::new(),
+            draw_pile : Vec::<card::Card>::new(),
         };
 
         // Generate the different color stacks
@@ -24,11 +31,34 @@ impl Playground{
             p.color_stacks.insert(c, Stack{color : c, value : 0});
         }
 
+        // Populate the draw pile
+        p.draw_pile = Playground::full_deck();
+
         p
     }
 
     pub fn can_play_card(&self, card : &card::Card) -> bool{
         let stack = self.color_stacks.get(&card.color).unwrap();
         stack.value + 1 == card.value
+    }
+
+    fn full_deck() -> Vec<card::Card>{
+        let deck = Vec::<card::Card>::new();
+        let card_nb = vec![3,2,2,2,1];
+
+        for c in card::Color::iter(){
+            for (card_nb, nb_copies) in card_nb.iter().enumerate(){
+                let copies = {
+                    if c == card::Color::All { 1 }
+                    else { *nb_copies }
+                };
+
+                for _ in 1..=copies{
+                    deck.push(card::Card {color : c, value : card_nb as u8, hints : (None, None)})
+                }
+            }
+        }
+
+        deck
     }
 }
